304cc3e24705d9f2d31681eea4183c355d8600e1
Al Gabriel Orig <jaworski.orig@gmail.com>
2025-02-26 15:02:36 +0800

    change model

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash-8b",
+    model_name="gemini-1.5-pro",
     generation_config=generation_config,
     system_instruction="always based on the Handbook.pdf and be nice.",
 )
